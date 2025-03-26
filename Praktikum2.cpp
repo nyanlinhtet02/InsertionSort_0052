aed1b80db6366b081596ecaedb139beb03d2be6a
nyanlinhtet02 <nyan.lin.ft24@mail.umy.ac.id>
2025-03-25 23:10:14 -0700

    Validasi input lumlaj Elemen Array dengan Batas Maksimal 20

--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -4,3 +4,16 @@ using namespace std;
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat Variable inputan n
 
+void input() {
+    // Procedure Input
+    while (true) {
+        cout << "Masukkan Jumlah Data pada Array: "; // Membuat Inputan jumlah element Array
+        cin >> n; // Memanggil variable inputan n
+
+        if (n <= 20) {
+            // Membuat Kondisi n tidak lebih dari 20
+            break;
+        } else {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; // Menampilkan Pesan jika data lebih dari 20
+        }
+    }
