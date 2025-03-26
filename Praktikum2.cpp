7169094aa30f1343fc46754407ce60da83af9e41
nyanlinhtet02 <nyan.lin.ft24@mail.umy.ac.id>
2025-03-25 23:16:21 -0700

    untuk Menampilkan Hasil Pengurutan

--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -45,3 +45,16 @@ void insertionSort() {
         arr[j + 1] = temp; // Step 5
     }
 }
+
+void display() {
+    cout << endl; // Output baris kosong
+    cout << "=====================" << endl;
+    cout << "Total Pass = " << n - 1 << endl; // Count element movement 
+    cout << "=====================" << endl;  // Output ke layar
+    cout << "Element Array yang telah tersusun" << endl; // Output ke layar
+    cout << "=====================" << endl;  // Output ke layar
+
+    for (int j = 0; j < n; j++) { // Looping dengan j dimulai dari 0 hingga n-1 
+        cout << arr[j] << " "; // Output ke layar
+
+        
