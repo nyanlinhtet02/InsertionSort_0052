237b7df55278de71ea3669c51b6ece09db938adb
nyanlinhtet02 <nyan.lin.ft24@mail.umy.ac.id>
2025-03-25 23:17:33 -0700

    untuk Menampilkan Flemen Array yang lelah lersusun

--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -57,4 +57,10 @@ void display() {
     for (int j = 0; j < n; j++) { // Looping dengan j dimulai dari 0 hingga n-1 
         cout << arr[j] << " "; // Output ke layar
 
-        
+        for (int j = 0; j < n; j++) { // Looping dengan j dimulai dari 0 hingga n-1 
+            cout << arr[j] << " "; // Output ke layar
+        }
+        cout << endl; // Output baris kosong 
+    }
+
+    
